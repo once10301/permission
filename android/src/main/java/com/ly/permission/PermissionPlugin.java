b0782cac4c7b6b6849da4b11ddf7b64b1308ae99
Andrei Jiácomo Zuse <andrei@amo.delivery>
2019-01-30 11:43:22 -0200

    fix: migrated to Android X

--- android/src/main/java/com/ly/permission/PermissionPlugin.java
+++ android/src/main/java/com/ly/permission/PermissionPlugin.java
@@ -7,9 +7,8 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.Settings;
 
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
